3b3de9b26bdf86a98ac0b8b96ca2cd4ba4ea78dd
Deepak K Bhaskar <deepucsait@gmail.com>
2017-01-19 09:48:09 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(window).load(function(){
-	$('#loader').delay( 500 ).fadeIn(500).hide(0);
-	$('.container').delay( 500 ).fadeIn(500).show(0);
+	$('#loader').fadeIn(500).hide(0);
+	$('.container').fadeIn(600).show(0);
 	
 });
